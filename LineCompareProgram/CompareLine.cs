55cfcc272959b7d7785fe55068ac07b741ba7f97
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-03 21:03:36 +0530

    Two lines eqal or not

---
+++ LineCompareProgram/CompareLine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineCompareProgram
+{
+    public class CompareLine
+    {
+        public void ComparingLines()
+        {
+            int x1 = 3;
+            int x2 = 6;
+            int y1 = 3;
+            int y2 = 7;
+            int x3 = 8;
+            int y3 = 9;
+            int x4 = 3;
+            int y4 = 2;
+
+            if (x2 == x4 && y2 == y4)
+            {
+                Console.WriteLine("The two lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Not equal");
+            }
+
+
+
+        }
+    }
+}
+    
+
---
+++ LineCompareProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LineCompareProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CompareLine compareLine = new CompareLine();
+            compareLine.ComparingLines();
+
+        }
+    }
+}
