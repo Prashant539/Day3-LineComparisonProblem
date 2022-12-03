9962f3258bcc489d6c4a3263385c5347cba62f6f
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-03 20:54:59 +0530

    Calculate Line Length

---
+++ CalculateLengthProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CalculateLengthProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateLength calculateLength = new CalculateLength();
+            calculateLength.CheckLength();
+
+        }
+    }
+}
